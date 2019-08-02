a10a05ee5f36155ae8a8b7d91d1bef0e4f57868c
Carlos Loureda <balanzeneto@gmail.com>
2019-08-02 03:49:40 +0200

    feat: Added example editor + results.
    I implemented ReactACE with #markown style for the editor .
    For the results added ReactMarkdown with  Prisma dark design.
    
    More info on task: https://www.notion.so/Descubrir-como-editar-Markdown-y-mostrarlo-eba9210680bb423fb5f724639383960d

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -3,40 +3,53 @@ import AceEditor from "react-ace";
 import "brace/mode/markdown";
 import "brace/theme/twilight";
 import ReactMarkdown from "react-markdown";
-
 import CodeBlock from "./CodeBlock";
 
 const CodeEditor = () => {
-  const [code, setCode] = React.useState("#Aqui va el Markdown");
+  const [code, setCode] = React.useState("# Aquí va el Markdown");
   const onChange = newValue => {
     setCode(newValue);
   };
 
   return (
-    <React.Fragment>
-      <h1>ACE in Action</h1>
-      <div>
-        <AceEditor
-          mode="markdown"
-          theme="twilight"
-          onChange={onChange}
-          name="brace-editor"
-          height="200px"
-          width="700px"
-          fontSize="20"
-          editorProps={{ $blockScrolling: true }}
-          value={code}
-        />
-      </div>
-      <div>
-        {/* {text} */}
+    <div style={classes.container}>
+      <div style={{ width: "50%" }}>
+        <h1>ACE in Action</h1>
         <div>
-          {/* TODO: Hacer que el codigo se vea bien ya en el formulario que montaré */}
-          <ReactMarkdown source={code} renderers={{ code: CodeBlock }} />
+          <AceEditor
+            mode="markdown"
+            theme="twilight"
+            onChange={onChange}
+            name="brace-editor"
+            height="300px"
+            width="100%"
+            fontSize="20"
+            editorProps={{ $blockScrolling: true }}
+            value={code}
+          />
         </div>
       </div>
-    </React.Fragment>
+      <div style={{ width: "50%" }}>
+        <div>
+          <h1>Resultado</h1>
+          {/* TODO: Hacer que el codigo se vea bien ya en el formulario que montaré */}
+          <ReactMarkdown
+            source={code}
+            renderers={{ code: CodeBlock }}
+            escapeHtml={false}
+          />
+        </div>
+      </div>
+    </div>
   );
+};
+
+const classes = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 };
 
 export default CodeEditor;
