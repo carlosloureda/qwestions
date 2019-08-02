c0c254aa882b542cd842054037f31a188ce0207c
Carlos Loureda <balanzeneto@gmail.com>
2019-08-02 05:45:33 +0200

    feat: WIP. Created basic form for adding question.
    Adde CodeEditor and select for type of answers.
    Began the creation of input answers, also in WIP.
    
    Design peding, just using Material Components without a design in mind

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,12 +1,107 @@
 import React from "react";
 import CodeEditor from "./CodeEditor";
 
-const AddQuestion = () => {
-  return (
-    <div>
-      <p>Add Question Page</p>
-      <CodeEditor />
+import { makeStyles } from "@material-ui/core/styles";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
 
+const AddQuestion = () => {
+  // let answer_model = { answer: "", isValid: false };
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    answered_types: "",
+    answers_inputs: [{}, {}] // TODO: meterlo con ids
+  });
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+  function handleChange(event) {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value
+    }));
+  }
+  const handleAnswersInputs = (e, index) => {
+    let inputValue = e.target.value;
+    setValues(oldValues => {
+      let _arr = [...oldValues.answers_inputs];
+      _arr[index] = { answer: inputValue, isValid: false };
+      return {
+        ...oldValues,
+        answers_inputs: _arr
+      };
+    });
+  };
+
+  const addNewAnswerLine = () => {
+    setValues(oldValues => {
+      return {
+        ...oldValues,
+        answers_inputs: oldValues.answers_inputs.concat([{}])
+      };
+    });
+  };
+
+  return (
+    <div style={{ width: "50%" }}>
+      {/* 1. Question editor respuestas: - tipos: - respuesta Multiple - code - */}
+      <h1 style={{ border: "1px solid" }}>Question editor</h1>
+      <CodeEditor width={98} />
+
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel ref={inputLabel} htmlFor="answered_types">
+          Answer type
+        </InputLabel>
+        <Select
+          value={values.answered_types}
+          onChange={handleChange}
+          input={
+            <OutlinedInput labelWidth={labelWidth} name="answered_types" />
+          }
+        >
+          <MenuItem value={"mutiple_choice"}>Mutiple Choice</MenuItem>
+          <MenuItem value={"code"}>Code</MenuItem>
+          <MenuItem value={"input"}>Input</MenuItem>
+          <MenuItem value={"textarea"}>Textarea</MenuItem>
+        </Select>
+      </FormControl>
+      <div>
+        {values.answered_types === "mutiple_choice" && (
+          <div>
+            <p>Answers: </p>
+            {values.answers_inputs.map((answer_input, index) => (
+              <div key={index}>
+                <TextField
+                  label="Name"
+                  className={classes.textField}
+                  value={answer_input.name}
+                  onChange={e => handleAnswersInputs(e, index)}
+                  margin="normal"
+                />
+                {/* TODO: the <div /> */}
+              </div>
+            ))}
+            {/* TODO: the <div /> */}
+            <div />
+            <button onClick={addNewAnswerLine}>Add Answer</button>
+          </div>
+        )}
+
+        {values.answered_types === "code" && <div>Mostrar code</div>}
+      </div>
+
+      <h2>Preview</h2>
+      {values.answered_types === "textarea" && <div>Mostrtar textarea</div>}
+      {values.answered_types === "input" && <div>Mostrar input</div>}
+      {/* input - TextArea */}
       {/*
         -  Titulo de la pregunta, va en el Markdown:
             - Label o titulo
@@ -30,3 +125,17 @@ const AddQuestion = () => {
   );
 };
 export default AddQuestion;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -2,43 +2,33 @@ import React from "react";
 import AceEditor from "react-ace";
 import "brace/mode/markdown";
 import "brace/theme/twilight";
-import ReactMarkdown from "react-markdown";
-import CodeBlock from "./CodeBlock";
 
-const CodeEditor = () => {
+const CodeEditor = ({ width, height }) => {
   const [code, setCode] = React.useState("# Aquí va el Markdown");
   const onChange = newValue => {
     setCode(newValue);
   };
-
+  console.log("width: ", width);
   return (
     <div style={classes.container}>
-      <div style={{ width: "50%" }}>
-        <h1>ACE in Action</h1>
-        <div>
-          <AceEditor
-            mode="markdown"
-            theme="twilight"
-            onChange={onChange}
-            name="brace-editor"
-            height="300px"
-            width="100%"
-            fontSize="20"
-            editorProps={{ $blockScrolling: true }}
-            value={code}
-          />
-        </div>
-      </div>
-      <div style={{ width: "50%" }}>
-        <div>
-          <h1>Resultado</h1>
-          {/* TODO: Hacer que el codigo se vea bien ya en el formulario que montaré */}
-          <ReactMarkdown
-            source={code}
-            renderers={{ code: CodeBlock }}
-            escapeHtml={false}
-          />
-        </div>
+      <div
+        style={{
+          width: width ? `${width}%` : "100%",
+          paddingLeft: "2%",
+          paddingBottom: "0.5em"
+        }}
+      >
+        <AceEditor
+          mode="markdown"
+          theme="twilight"
+          onChange={onChange}
+          name="brace-editor"
+          height={height ? `${height}%` : "300px"}
+          width={width ? `${width}%` : "100%"}
+          fontSize="20"
+          editorProps={{ $blockScrolling: true }}
+          value={code}
+        />
       </div>
     </div>
   );
@@ -47,8 +37,8 @@ const CodeEditor = () => {
 const classes = {
   container: {
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between"
+    flexDirection: "row"
+    // alignContent: "center"
   }
 };
 
